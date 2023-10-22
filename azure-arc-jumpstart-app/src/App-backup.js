6d24d468378cd9210a0651fa5470e2016da7545f
AntonBurschAtFractalAI <anton.bursch@fractal.ai>
2023-10-22 09:37:44 -0700

    refactoring app

---
+++ azure-arc-jumpstart-app/src/App-backup.js
@@ -0,0 +1,250 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import NavBar from './components/navigation/NavBar';
+import BreadcrumbBar from './components/navigation/breadcrumbs/BreadcrumbBar';
+import MenuDrawer from './components/navigation/menu/MenuDrawer';
+import Dropdown from './components/navigation/dropdown/Dropdown';
+import { SideMenu } from './components/navigation/sidemenu/SideMenu';
+import Doc from './components/Doc';
+
+const App = () => {
+  const [menuItems, setMenuItems] = useState([]);
+  const [selectedMenuItem, setSelectedMenuItem] = useState(null);
+  const [sideMenuItem, setSideMenuItem] = useState(null);
+  const [selectedSideMenuItem, setSelectedSideMenuItem] = useState(null);
+  const [markdownFileContents, setMarkdownFileContents] = useState('');
+  const [markdownFilePath, setMarkdownFilePath] = useState('');
+  const [bookmarks, setBookmarks] = useState([]);
+
+  useEffect(() => {
+    const fetchMenuDrawerData = async () => {
+      const response = await fetch('./menu-drawer.json');
+      const menuItems = await response.json();
+      setMenuItems(menuItems);
+    };
+
+    const fetchSideMenuData = async () => {
+      const response = await fetch('./side-menu.json');
+      const data = await response.json();
+      sortNodeTree(data);
+      setSideMenuItem(data);
+    };
+
+    fetchMenuDrawerData();
+    fetchSideMenuData();
+  }, []);
+
+  const removeFrontmatter = (text) => {
+    const frontmatterPattern = /^---\s*[\s\S]*?---\s*/;
+    const textWithoutFrontmatter = text.replace(frontmatterPattern, '');
+
+    return textWithoutFrontmatter;
+  }
+
+  const handleFileFetch = (pagePath) => {
+    const pagePathSplit = pagePath.split('#');
+    setMarkdownFilePath(pagePathSplit);
+    getDoc(pagePathSplit);
+  }
+
+  const getDoc = async (path) => {
+    try {
+      const pathWithForwardSlashes = path[0].replace(/\\/g, '/');
+      const baseUrl = 'https://raw.githubusercontent.com/Azure/arc_jumpstart_docs/main';
+      let fullPath = path[0] === '' ? `${baseUrl}/docs/_index.md` : `${baseUrl}/docs/${pathWithForwardSlashes}/_index.md`;
+      if (path.length > 1) {
+        fullPath = `${fullPath}#${path[1]}`;
+      }
+      const res = await fetch(fullPath);
+      const doc = await res.text();
+      const htmlText = removeFrontmatter(doc);
+      if (fullPath.includes('azure_arc_jumpstart/')) {
+        const node = findNode(sideMenuItem, path[0]);
+        setSelectedSideMenuItem((prev) => {
+          if (node && node.children && node.children.length > 0) {
+            let htmlContent = createHtml(node);
+            // for each child
+            node.children.forEach((child) => {
+              htmlContent = `${htmlContent}${createHtml(child)}`;
+            });
+
+            setMarkdownFileContents(htmlContent);
+          } else {
+            setMarkdownFileContents(htmlText);
+          }
+
+          return node;
+        });
+      } else {
+        setMarkdownFileContents(htmlText);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const createHtml = (node) => {
+    let html = '';
+    if (node.frontMatter) {
+      if (node.frontMatter.title) {
+        html = `<a href='${node.path}'>${node.frontMatter.title}</a>`;
+      } else if (node.frontMatter.linkTitle) {
+        html = `<a href='${node.path}'>${node.frontMatter.linkTitle}</a>`;
+      }
+      if (node.frontMatter.description) {
+        html = `${html}<p>${node.frontMatter.description}</p>`;
+      } else {
+        html = `${html}<p></p>`;
+      }
+    }
+    return html;
+  }
+
+  const findNode = (node, path) => {
+    if (node.path.replace('\\', '/') === path.replace('\\', '/')) {
+      return node;
+    } else if (node.children) {
+      let result = null;
+      for (let i = 0; result == null && i < node.children.length; i++) {
+        result = findNode(node.children[i], path);
+      }
+      return result;
+    }
+    return null;
+  }
+
+  const sortNodeTree = (node) => {
+    if (node.children) {
+      node.children.sort((a, b) => {
+        const aWeight = a.frontMatter && a.frontMatter.weight ? a.frontMatter.weight : 0;
+        const bWeight = b.frontMatter && b.frontMatter.weight ? b.frontMatter.weight : 0;
+        if (aWeight < bWeight) {
+          return -1;
+        } else if (aWeight > bWeight) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+      node.children.forEach((child) => {
+        sortNodeTree(child);
+      });
+    }
+  }
+
+  const onChange = (e) => {
+    const hash = e.target.value;
+    const targetElement = document.getElementById(hash);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  const gatherElementIds = () => {
+    const elementIds = [];
+
+    const docElement = document.getElementById('doc');
+    const elementsWithIds = docElement.querySelectorAll('[id]');
+
+    elementsWithIds.forEach((element) => {
+      elementIds.push(element.id);
+    });
+
+    setBookmarks(elementIds);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '300px auto',
+          justifyContent: 'space-between',
+          zIndex: '-1'
+        }}
+      >
+        <span
+          style={{
+            position: 'absolute',
+            top: '96px',
+            left: '0px',
+            bottom: '0px',
+            // scroll without visible scroll bars
+            overflowY: 'scroll',
+            // hide scroll bars
+            msOverflowStyle: 'none',
+            scrollbarWidth: 'none',
+            '&::-webkit-scrollbar': {
+              display: 'none'
+            }
+          }}
+        >
+          {
+            sideMenuItem && (
+              <SideMenu
+                sideMenuItem={sideMenuItem}
+                handleFileFetch={handleFileFetch}
+              />
+            )
+          }
+        </span>
+        <span
+          id="doc"
+          style={{
+            position: 'absolute',
+            top: '96px',
+            left: '300px',
+            right: '10px',
+            bottom: '0px',
+            color: 'white',
+            overflowY: 'scroll',
+            // hide scroll bars
+            msOverflowStyle: 'none',
+            scrollbarWidth: 'none',
+            '&::-webkit-scrollbar': {
+              display: 'none'
+            }
+          }}>
+          <Doc
+            doc={markdownFileContents}
+            path={markdownFilePath}
+            handleFileFetch={handleFileFetch}
+            gatherElementIds={gatherElementIds}
+          />
+        </span>
+      </div>
+      <NavBar
+        menuItems={menuItems}
+        selectedMenuItem={selectedMenuItem}
+        setSelectedMenuItem={setSelectedMenuItem}
+      />
+      {
+        selectedMenuItem ? (
+          <MenuDrawer
+            menuItem={selectedMenuItem}
+            handleFileFetch={handleFileFetch}
+          />
+        ) : (
+          <>
+            <BreadcrumbBar
+              path={markdownFilePath}
+              handleFileFetch={handleFileFetch}
+            />
+            <span style={{ position: 'absolute', top: '48px', right: '71px' }}>
+              <Dropdown
+                bookmarks={bookmarks}
+                markdownFilePath={markdownFilePath}
+                handleFileFetch={handleFileFetch}
+              >
+                Jump to section
+              </Dropdown>
+            </span>
+          </>
+        )
+      }
+    </>
+  );
+}
+
+export default App;
--- azure-arc-jumpstart-app/src/App.js
+++ azure-arc-jumpstart-app/src/App.js
@@ -1,252 +1,212 @@
-import React, { useEffect, useState } from 'react';
+
+import React, { useState, useEffect, useRef } from 'react';
+import { BrowserRouter, Routes, Route, Link, useLocation, useRoutesMatch } from 'react-router-dom';
+import Markdown from 'markdown-to-jsx';
 import './App.css';
-import NavBar from './components/navigation/NavBar';
-import BreadcrumbBar from './components/navigation/breadcrumbs/BreadcrumbBar';
-import MenuDrawer from './components/navigation/menu/MenuDrawer';
-import Dropdown from './components/navigation/dropdown/Dropdown';
-import { SideMenu } from './components/navigation/sidemenu/SideMenu';
-import Doc from './components/Doc';
 
-const App = () => {
-  const [menuItems, setMenuItems] = useState([]);
-  const [selectedMenuItem, setSelectedMenuItem] = useState(null);
-  const [sideMenuItem, setSideMenuItem] = useState(null);
-  const [selectedSideMenuItem, setSelectedSideMenuItem] = useState(null);
-  const [markdownFileContents, setMarkdownFileContents] = useState('');
-  const [markdownFilePath, setMarkdownFilePath] = useState('');
-  const [bookmarks, setBookmarks] = useState([]);
 
-  useEffect(() => {
-    const fetchMenuDrawerData = async () => {
-      const response = await fetch('./menu-drawer.json');
-      const menuItems = await response.json();
-      setMenuItems(menuItems);
-    };
+function Home({ updateSections }) {
+    useEffect(() => {
+        updateSections();
+    }, []);
+    return <h2>Home Page</h2>;
+}
 
-    const fetchSideMenuData = async () => {
-      const response = await fetch('./side-menu.json');
-      const data = await response.json();
-      sortNodeTree(data);
-      setSideMenuItem(data);
-    };
+function MarkdownPage({ path, updateSections }) {
+    const [markdown, setMarkdown] = useState('');
 
-    fetchMenuDrawerData();
-    fetchSideMenuData();
-  }, []);
-
-  const removeFrontmatter = (text) => {
-    const frontmatterPattern = /^---\s*[\s\S]*?---\s*/;
-    const textWithoutFrontmatter = text.replace(frontmatterPattern, '');
-
-    return textWithoutFrontmatter;
-  }
-
-  const handleFileFetch = (pagePath) => {
-    const pagePathSplit = pagePath.split('#');
-    setMarkdownFilePath(pagePathSplit);
-    getDoc(pagePathSplit);
-  }
-
-  const getDoc = async (path) => {
-    try {
-      const pathWithForwardSlashes = path[0].replace(/\\/g, '/');
-      const baseUrl = 'https://raw.githubusercontent.com/Azure/arc_jumpstart_docs/main';
-      let fullPath = path[0] === '' ? `${baseUrl}/docs/_index.md` : `${baseUrl}/docs/${pathWithForwardSlashes}/_index.md`;
-      if (path.length > 1) {
-        fullPath = `${fullPath}#${path[1]}`;
-      }
-      const res = await fetch(fullPath);
-      const doc = await res.text();
-      const htmlText = removeFrontmatter(doc);
-      if (fullPath.includes('azure_arc_jumpstart/')) {
-        const node = findNode(sideMenuItem, path[0]);
-        setSelectedSideMenuItem((prev) => {
-          if (node && node.children && node.children.length > 0) {
-            console.log(node.children.length);
-            let htmlContent = '';
-            // for each child
-            node.children.forEach((child) => {
-              htmlContent = `${htmlContent}${createHtml(child)}`;
-            });
-
-            console.log(htmlContent);
-            setMarkdownFileContents(htmlContent);
-          } else {
-            setMarkdownFileContents(htmlText);
-          }
-
-          return node;
-        });
-      } else {
-        setMarkdownFileContents(htmlText);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  const createHtml = (node) => {
-    let html = '';
-    if (node.frontMatter) {
-      if (node.frontMatter.title) {
-        html = `<a href='${node.path}'>${node.frontMatter.title}</a>`;
-      } else if (node.frontMatter.linkTitle) {
-        html = `<a href='${node.path}'>${node.frontMatter.linkTitle}</a>`;
-      }
-      if (node.frontMatter.description) {
-        html = `${html}<p>${node.frontMatter.description}</p>`;
-      } else {
-        html = `${html}<p></p>`;
-      }
-    }
-    return html;
-  }
-
-  const findNode = (node, path) => {
-    if (node.path.replace('\\', '/') === path.replace('\\', '/')) {
-      return node;
-    } else if (node.children) {
-      let result = null;
-      for (let i = 0; result == null && i < node.children.length; i++) {
-        result = findNode(node.children[i], path);
-      }
-      return result;
-    }
-    return null;
-  }
-
-  const sortNodeTree = (node) => {
-    if (node.children) {
-      node.children.sort((a, b) => {
-        const aWeight = a.frontMatter && a.frontMatter.weight ? a.frontMatter.weight : 0;
-        const bWeight = b.frontMatter && b.frontMatter.weight ? b.frontMatter.weight : 0;
-        if (aWeight < bWeight) {
-          return -1;
-        } else if (aWeight > bWeight) {
-          return 1;
-        } else {
-          return 0;
+    useEffect(() => {
+        async function getMarkdown() {
+            const basePath = 'https://raw.githubusercontent.com/Azure/arc_jumpstart_docs/main/docs/';
+            const fullPath = `${basePath}${path}/_index.md`;
+            const response = await fetch(fullPath);
+            const text = await response.text();
+            setMarkdown((text));
         }
-      });
-      node.children.forEach((child) => {
-        sortNodeTree(child);
-      });
+
+        getMarkdown();
+    }, [path]);
+
+    useEffect(() => {
+        if (markdown) {
+            updateSections();
+        }
+    }, [markdown]);
+
+    return <Markdown>{markdown}</Markdown>;
+}
+
+function Breadcrumbs() {
+    const location = useLocation();
+
+    // Split the path
+    const pathnames = location.pathname.split('/').filter(x => x);
+
+    // display pathname with Capitalized first letter
+    const capitalize = (s) => {
+        return s.charAt(0).toUpperCase() + s.slice(1);
     }
-  }
 
-  const onChange = (e) => {
-    const hash = e.target.value;
-    const targetElement = document.getElementById(hash);
+    return (
+        <div>
+            <Link to="/">Home</Link>
+            {pathnames.map((value, index) => {
+                const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
+                return (
+                    <span key={index}>
+                        {' > '}
+                        <Link to={routeTo}>{capitalize(value)}</Link>
+                    </span>
+                );
+            })}
+        </div>
+    );
+}
 
-    if (targetElement) {
-      targetElement.scrollIntoView({ behavior: 'smooth' });
+function Dropdown({ items }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className="dropdown">
+            <button className="dropdown-toggle" onClick={() => setIsOpen(!isOpen)}>
+                Select a section
+            </button>
+            {isOpen && (
+                <div className="dropdown-menu">
+                    {items.map(item => (
+                        <a href={item.href} className="dropdown-item">
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+}
+
+function TreeView({ node, level = 0 }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const hasChildren = node.children && node.children.length > 0;
+
+    const parseTitle = (node) => {
+        if(node.frontMatter) {
+            if(node.frontMatter.title) {
+                return node.frontMatter.title;
+            } else if(node.frontMatter.linkTitle) {
+                return node.frontMatter.linkTitle;
+            }
+        }
+        return node.path;
     }
-  }
 
-  const gatherElementIds = () => {
-    const elementIds = [];
+    return (
+        <ul style={{ marginLeft: level }}>
+            <li key={node.path}>
+                {hasChildren && (
+                    <span onClick={() => setIsOpen(!isOpen)}>
+                        {isOpen ? '-' : '+'}
+                    </span>
+                )}
+                <Link to={node.path}>{parseTitle(node)}</Link>
 
-    const docElement = document.getElementById('doc');
-    const elementsWithIds = docElement.querySelectorAll('[id]');
+                {hasChildren && isOpen && (
+                    <ul>
+                        {node.children.map(child => (
+                            <TreeView node={child} key={child.path} level={level + 1} />
+                        ))}
+                    </ul>
+                )}
+            </li>
+        </ul>
+    );
+};
 
-    elementsWithIds.forEach((element) => {
-      elementIds.push(element.id);
-    });
+function App() {
+    const [node, setNode] = useState({});
+    const [dynamicRoutes, setDynamicRoutes] = useState([]);
+    const [sections, setSections] = useState([]);
+    const pageRef = useRef(null);
 
-    setBookmarks(elementIds);
-  };
+    const extractRoutes = (node, arr = []) => {
+        if (!node.path.endsWith('\\img') && !node.path.endsWith('\\docs')) {
+            arr.push({
+                path: node.path.replace(/\\/g, '/'),
+                title: node.path
+            });
+        }
 
-  return (
-    <>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '300px auto',
-          justifyContent: 'space-between',
-          zIndex: '-1'
-        }}
-      >
-        <span
-          style={{
-            position: 'absolute',
-            top: '96px',
-            left: '0px',
-            bottom: '0px',
-            // scroll without visible scroll bars
-            overflowY: 'scroll',
-            // hide scroll bars
-            msOverflowStyle: 'none',
-            scrollbarWidth: 'none',
-            '&::-webkit-scrollbar': {
-              display: 'none'
-            }
-          }}
-        >
-          {
-            sideMenuItem && (
-              <SideMenu
-                sideMenuItem={sideMenuItem}
-                handleFileFetch={handleFileFetch}
-              />
-            )
-          }
-        </span>
-        <span
-          id="doc"
-          style={{
-            position: 'absolute',
-            top: '96px',
-            left: '300px',
-            right: '10px',
-            bottom: '0px',
-            color: 'white',
-            overflowY: 'scroll',
-            // hide scroll bars
-            msOverflowStyle: 'none',
-            scrollbarWidth: 'none',
-            '&::-webkit-scrollbar': {
-              display: 'none'
-            }
-          }}>
-          <Doc
-            doc={markdownFileContents}
-            path={markdownFilePath}
-            handleFileFetch={handleFileFetch}
-            gatherElementIds={gatherElementIds}
-          />
-        </span>
-      </div>
-      <NavBar
-        menuItems={menuItems}
-        selectedMenuItem={selectedMenuItem}
-        setSelectedMenuItem={setSelectedMenuItem}
-      />
-      {
-        selectedMenuItem ? (
-          <MenuDrawer
-            menuItem={selectedMenuItem}
-            handleFileFetch={handleFileFetch}
-          />
-        ) : (
-          <>
-            <BreadcrumbBar
-              path={markdownFilePath}
-              handleFileFetch={handleFileFetch}
-            />
-            <span style={{ position: 'absolute', top: '48px', right: '71px' }}>
-              <Dropdown
-                bookmarks={bookmarks}
-                markdownFilePath={markdownFilePath}
-                handleFileFetch={handleFileFetch}
-              >
-                Jump to section
-              </Dropdown>
-            </span>
-          </>
-        )
-      }
-    </>
-  );
+        if (node.children && node.children.length > 0) {
+            node.children.forEach(child => extractRoutes(child, arr));
+        }
+
+        return arr;
+    };
+
+    useEffect(() => {
+        const fetchSideMenu = async () => {
+            const response = await fetch('./side-menu.json');
+            const data = await response.json();
+            const extractedRoutes = extractRoutes(data);
+            console.log(extractedRoutes);
+            setDynamicRoutes(extractedRoutes);
+            setNode(data);
+        };
+
+        fetchSideMenu();
+    }, []);
+
+    const updateSections = () => {
+        if (pageRef.current) {
+            const elementsWithId = pageRef.current.querySelectorAll('[id]');
+            const discoveredSections = Array.from(elementsWithId).map(el => ({
+                id: el.id,
+                name: el.getAttribute('data-name') || el.id,
+                href: `#${el.id}`
+            }));
+            setSections(discoveredSections);
+        }
+    }
+
+    return (
+        <BrowserRouter>
+            <div ref={pageRef}>
+
+                <hr />
+
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-between'
+                    }}
+                >
+                    <Breadcrumbs />
+                    <Dropdown items={sections} />
+                </div>
+
+                <hr />
+
+                <div
+                    style={{
+                        display: 'grid',
+                        gridTemplateColumns: '300px auto',
+                    }}
+                >
+                    <TreeView node={node} />
+                    <Routes>
+                        <Route path="/" element={<Home updateSections={updateSections} />} />
+                        {dynamicRoutes.map(route => (
+                            <Route
+                                key={route.path}
+                                path={route.path}
+                                element={<MarkdownPage path={route.path} updateSections={updateSections} />}
+                            />
+                        ))}
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
