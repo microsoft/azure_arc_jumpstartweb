64cfb662651bfcc53da2840b279e68d10298f758
MelissaBurschAtFractal <melissa.bursch@fractal.ai>
2023-10-22 22:02:00 -0700

    adding side nav states

---
+++ azure-arc-jumpstart-app/src/components/navigation/sidemenu/MenuSectionHeaderClosed.jsx
@@ -0,0 +1,14 @@
+import "./MenuSectionHeaderClosed.css";
+
+const MenuSectionHeaderClosed = ({ ...props }) => {
+  return (
+    <div className="menu-section-header-closed">
+      <div className="menu-section-header-closed-left-lockup">
+        <div className="menu-section-header-closed-header-text">AI and Machine Learning </div>
+      </div>
+    </div>
+  );
+};
+
+export default MenuSectionHeaderClosed;
+
