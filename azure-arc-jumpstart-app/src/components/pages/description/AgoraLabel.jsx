fca2cca50c17c5e9e0b67fe35ef413ad2c678b30
MelissaBurschAtFractal <melissa.bursch@fractal.ai>
2023-10-23 19:00:08 -0700

    added more styles to pages

---
+++ azure-arc-jumpstart-app/src/components/pages/description/AgoraLabel.jsx
@@ -0,0 +1,11 @@
+import "./AgoraLabel.css";
+
+const AgoraLabel = ({ ...props }) => {
+  return <div className="agora-label">Agora </div>;
+};
+
+export default AgoraLabel;
+
+
+
+
---
+++ azure-arc-jumpstart-app/src/components/pages/description/HCIBoxLabel.jsx
@@ -0,0 +1,8 @@
+import "./HCIBoxLabel.css";
+
+const HCIBoxLabel = ({ ...props }) => {
+  return <div className="hci-box-label">HCIBox </div>;
+};
+
+export default HCIBoxLabel;
+
