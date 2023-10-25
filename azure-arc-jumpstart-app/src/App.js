28a3d836adf0ba01da2a94efbccdf3c995caf045
AntonBurschAtFractal <anton.bursch@fractal.ai>
2023-10-24 19:38:25 -0700

    quick fix to side-menu background color (removed temp dev color)

--- azure-arc-jumpstart-app/src/App.js
+++ azure-arc-jumpstart-app/src/App.js
@@ -63,7 +63,7 @@ function App() {
 
     const updateBreadcrumbs = (node) => {
         const breadcrumbs = [];
-
+    
         let currentNode = node;
         while (currentNode) {
             if (currentNode.frontMatter) {
@@ -103,7 +103,7 @@ function App() {
             setSections(discoveredSections);
         }
     }
-
+    
     const findNode = (node, path) => {
         if (node.path.replace('\\', '/') === path.replace('\\', '/')) {
             return node;
@@ -116,7 +116,7 @@ function App() {
         }
         return null;
     }
-
+    
     const locatation = useLocation();
 
     useEffect(() => {
@@ -124,150 +124,149 @@ function App() {
     }, [locatation]);
 
     return (
-        <div ref={pageRef}>
-            <div
-                style={{
-                    position: 'fixed',
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    height: '5vh',
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    padding: '0 20px',
-                    zIndex: 4
-                }}
-            >
-                <NavBar
-                    menuItems={menuItems}
-                    selectedMenuItem={selectedMenuItem}
-                    setSelectedMenuItem={toggleMenuDrawer}
-                />
-            </div>
-            <div
-                style={{
-                    background: '#0a0a0a',
-                    display: 'grid',
-                    gridTemplateColumns: 'auto auto',
-                    justifyContent: 'space-between',
-                    position: 'fixed',
-                    top: '48px',
-                    left: '0px',
-                    right: '0px',
-                    height: '52px',
-                    paddingLeft: '10px',
-                    paddingRight: '10px',
-                    zIndex: 1,
-                }}
-            >
-                <Breadcrumbs breadcrumbs={breadcrumbs} />
-                <Dropdown items={sections} />
-            </div>
-            <div
-                style={{
-                    position: 'fixed',
-                    top: isMenuDrawerOpen ? 0 : -300,
-                    left: 0,
-                    right: 0,
-                    height: 300,
-                    opacity: isMenuDrawerOpen ? 1 : 0,
-                    zIndex: 3,
-                    transition: 'all 0.5s'
-                }}
-            >
-                {selectedMenuItem && <MenuDrawer menuItem={selectedMenuItem} />}
-            </div>
-            <div
-                style={{
-                    position: 'fixed',
-                    top: 100,
-                    left: sideMenuLeft,
-                    backgroundColor: 'darkgray',
-                    width: 300,
-                    height: 'calc(100vh - 100px)',
-                    zIndex: 1,
-                    transition: 'all 0.5s',
-                }}
-            >
-                <SideMenu pathNode={currentPathNode}></SideMenu>
-                <span
+            <div ref={pageRef}>
+                <div
                     style={{
-                        position: 'absolute',
-                        bottom: 0,
-                        right: isSideMenuOpen ? 0 : -65,
-                        zIndex: 1,
-                        transition: 'right 0.5s',
+                        position: 'fixed',
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        height: '5vh',
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        padding: '0 20px',
+                        zIndex: 4
                     }}
-                    onClick={toggleSideMenu}
                 >
-                    <svg
+                    <NavBar
+                        menuItems={menuItems}
+                        selectedMenuItem={selectedMenuItem}
+                        setSelectedMenuItem={toggleMenuDrawer}
+                    />
+                </div>
+                <div
+                    style={{
+                        background: '#0a0a0a',
+                        display: 'grid',
+                        gridTemplateColumns: 'auto auto',
+                        justifyContent: 'space-between',
+                        position: 'fixed',
+                        top: '48px',
+                        left: '0px',
+                        right: '0px',
+                        height: '52px',
+                        paddingLeft: '10px',
+                        paddingRight: '10px',
+                        zIndex: 1,
+                    }}
+                >
+                    <Breadcrumbs breadcrumbs={breadcrumbs}/>
+                    <Dropdown items={sections} />
+                </div>
+                <div
+                    style={{
+                        position: 'fixed',
+                        top: isMenuDrawerOpen ? 0 : -300,
+                        left: 0,
+                        right: 0,
+                        height: 300,
+                        opacity: isMenuDrawerOpen ? 1 : 0,
+                        zIndex: 3,
+                        transition: 'all 0.5s'
+                    }}
+                >
+                    {selectedMenuItem && <MenuDrawer menuItem={selectedMenuItem} />}
+                </div>
+                <div
+                    style={{
+                        position: 'fixed',
+                        top: 100,
+                        left: sideMenuLeft,
+                        width: 300,
+                        height: 'calc(100vh - 100px)',
+                        zIndex: 1,
+                        transition: 'all 0.5s',
+                    }}
+                >
+                    <SideMenu pathNode={currentPathNode}></SideMenu>
+                    <span
                         style={{
-                            transform: isSideMenuOpen ? 'rotate(180deg)' : 'rotate(0deg)',
-                            transition: 'all 0.5s',
+                            position: 'absolute',
+                            bottom: 0,
+                            right: isSideMenuOpen ? 0 : -65,
+                            zIndex: 1,
+                            transition: 'right 0.5s',
                         }}
-                        width="32"
-                        height="24"
-                        viewBox="0 0 32 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
+                        onClick={toggleSideMenu}
                     >
-                        <path
-                            d="M19.7071 4.29289C20.0976 4.68342 20.0976 5.31658 19.7071 5.70711L13.4142 12L19.7071 18.2929C20.0976 18.6834 20.0976 19.3166 19.7071 19.7071C19.3166 20.0976 18.6834 20.0976 18.2929 19.7071L11.2929 12.7071C10.9024 12.3166 10.9024 11.6834 11.2929 11.2929L18.2929 4.29289C18.6834 3.90237 19.3166 3.90237 19.7071 4.29289Z"
-                            fill="white"
-                        />
-                    </svg>
-                </span>
-            </div>
-            <div
-                style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    marginTop: '100px',
-                    zIndex: -1
-                }}
-            >
+                        <svg
+                            style={{
+                                transform: isSideMenuOpen ? 'rotate(180deg)' : 'rotate(0deg)',
+                                transition: 'all 0.5s',
+                            }}
+                            width="32"
+                            height="24"
+                            viewBox="0 0 32 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M19.7071 4.29289C20.0976 4.68342 20.0976 5.31658 19.7071 5.70711L13.4142 12L19.7071 18.2929C20.0976 18.6834 20.0976 19.3166 19.7071 19.7071C19.3166 20.0976 18.6834 20.0976 18.2929 19.7071L11.2929 12.7071C10.9024 12.3166 10.9024 11.6834 11.2929 11.2929L18.2929 4.29289C18.6834 3.90237 19.3166 3.90237 19.7071 4.29289Z"
+                                fill="white"
+                            />
+                        </svg>
+                    </span>
+                </div>
                 <div
                     style={{
                         display: 'flex',
-                        position: 'relative'
+                        flexDirection: 'column',
+                        marginTop: '100px',
+                        zIndex: -1
                     }}
                 >
                     <div
                         style={{
-                            position: 'absolute',
-                            left: isSideMenuOpen ? 300 : 0,
-                            right: 0,
-                            overflow: 'auto',
-                            scrollbarWidth: 'none',
-                            msOverflowStyle: 'none',
-
-                            transition: 'all 0.5s'
+                            display: 'flex',
+                            position: 'relative'
                         }}
                     >
                         <div
                             style={{
-                                margin: '0 20px',
+                                position: 'absolute',
+                                left: isSideMenuOpen ? 300 : 0,
+                                right: 0,
+                                overflow: 'auto',
+                                scrollbarWidth: 'none',
+                                msOverflowStyle: 'none',
+
+                                transition: 'all 0.5s'
                             }}
                         >
-                            <Routes>
-                                <Route path="/" element={() => {
-                                    setCurrentPathNode({});
-                                    return <Home updateSections={updateSections} />
-                                }} />
-                                {dynamicRoutes.map(route => (
-                                    <Route
-                                        key={route.path}
-                                        path={route.path}
-                                        element={<MarkdownPage node={pathNode} path={route.path} updateBreadcrumbs={updateBreadcrumbs} updateSections={updateSections} />}
-                                    />
-                                ))}
-                            </Routes>
+                            <div
+                                style={{
+                                    margin: '0 20px',
+                                }}
+                            >
+                                <Routes>
+                                    <Route path="/" element={() => {
+                                        setCurrentPathNode({});
+                                        return <Home updateSections={updateSections} />
+                                    }} />
+                                    {dynamicRoutes.map(route => (
+                                        <Route
+                                            key={route.path}
+                                            path={route.path}
+                                            element={<MarkdownPage node={pathNode} path={route.path} updateBreadcrumbs={updateBreadcrumbs} updateSections={updateSections} />}
+                                        />
+                                    ))}
+                                </Routes>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
     );
 }
 
