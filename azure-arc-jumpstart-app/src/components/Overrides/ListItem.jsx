a4d938b1e8a4319dee81398a2dd6ee02144ebcda
AntonBurschAtFractal <anton.bursch@fractal.ai>
2023-10-25 19:03:07 -0700

    anchors with li fixed

--- azure-arc-jumpstart-app/src/components/Overrides/ListItem.jsx
+++ azure-arc-jumpstart-app/src/components/Overrides/ListItem.jsx
@@ -4,7 +4,7 @@ import './ListItem.css';
 export const ListItem = (props) => {
     return (
         <li {...props} className="custom-list-item">
-            <svg
+             {/* <svg
                 class="frame"
                 width="12"
                 height="25"
@@ -16,8 +16,8 @@ export const ListItem = (props) => {
                     d="M4 14.0625C4 12.9579 4.89543 12.0625 6 12.0625C7.10455 12.0625 8 12.9579 8 14.0625C8 15.167 7.10455 16.0625 6 16.0625C4.89543 16.0625 4 15.167 4 14.0625Z"
                     fill="white"
                 />
-            </svg>
-            {props.children}
+            </svg>  */}
         </li>
+
     )
 };
